8af5c874f5bb5d562eed329d83cd16fe1d0227db
Scott Vickers <svickers@Ratios-MacBook-Pro-2.local>
2015-06-24 16:33:39 -0700

    create top nav, put about in a template, add a few styles

--- src/AppBundle/Controller/AboutController.php
+++ src/AppBundle/Controller/AboutController.php
@@ -3,8 +3,8 @@
 namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Response;
 
 class AboutController extends Controller
 {
@@ -12,11 +12,10 @@ class AboutController extends Controller
 
     /**
      * @Route("/about", name="about")
+     * @Template
      */
     public function aboutAction()
     {
-        return new Response(
-            "<html><body>Symfony demo using the {$this->about}</body></html>"
-        );
+        return array('about'=>$this->about);
     }
 }

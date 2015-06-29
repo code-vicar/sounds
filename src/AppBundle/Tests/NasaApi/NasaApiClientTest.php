42ab84598c60a150a567aa7c049cdbe58c929abe
Scott Vickers <svickers@weareratio.com>
2015-06-29 15:55:51 -0700

    fixes spacing and missing new line at eof

--- src/AppBundle/Tests/NasaApi/NasaApiClientTest.php
+++ src/AppBundle/Tests/NasaApi/NasaApiClientTest.php
@@ -8,18 +8,18 @@ use AppBundle\Tests\Mocks\BuzzMock;
 class NasaApiClientTest extends \PHPUnit_Framework_TestCase
 {
 
-	private $nasaApiClient;
+    private $nasaApiClient;
 
-	function __construct() {
-		$buzz = new BuzzMock();
-		
-		$this->nasaApiClient = new NasaApiClient('123', '1234', $buzz);
-	}
+    function __construct() {
+        $buzz = new BuzzMock();
+        
+        $this->nasaApiClient = new NasaApiClient('123', '1234', $buzz);
+    }
 
-	/**
-	* Assert api_key is readable
-	*/
-	public function testReadApiKey()
+    /**
+    * Assert api_key is readable
+    */
+    public function testReadApiKey()
     {
         $this->assertEquals('123', $this->nasaApiClient->api_key);
     }
@@ -29,16 +29,16 @@ class NasaApiClientTest extends \PHPUnit_Framework_TestCase
     * @expectedException ReflectionException
     */
     public function testWriteApiKey() {
-    	$NasaApiClientReflection = new \ReflectionClass('AppBundle\NasaApi\NasaApiClient');
-    	
-    	$prop = $NasaApiClientReflection->getProperty('api_key');
-    	$prop->setValue('321');
+        $NasaApiClientReflection = new \ReflectionClass('AppBundle\NasaApi\NasaApiClient');
+        
+        $prop = $NasaApiClientReflection->getProperty('api_key');
+        $prop->setValue('321');
     }
 
     /**
-	* Assert soundcloud_client_id is readable
-	*/
-	public function testReadSoundcloudClientID()
+    * Assert soundcloud_client_id is readable
+    */
+    public function testReadSoundcloudClientID()
     {
         $this->assertEquals('1234', $this->nasaApiClient->soundcloud_client_id);
     }
@@ -48,16 +48,16 @@ class NasaApiClientTest extends \PHPUnit_Framework_TestCase
     * @expectedException ReflectionException
     */
     public function testWriteSoundcloudClientID() {
-    	$NasaApiClientReflection = new \ReflectionClass('AppBundle\NasaApi\NasaApiClient');
-    	
-    	$prop = $NasaApiClientReflection->getProperty('soundcloud_client_id');
-    	$prop->setValue('321');
+        $NasaApiClientReflection = new \ReflectionClass('AppBundle\NasaApi\NasaApiClient');
+        
+        $prop = $NasaApiClientReflection->getProperty('soundcloud_client_id');
+        $prop->setValue('321');
     }
 
     /**
-	* Assert getSounds is callable
-	*/
-	public function testGetSounds()
+    * Assert getSounds is callable
+    */
+    public function testGetSounds()
     {
         $this->assertEquals('https://api.nasa.gov/planetary/sounds?api_key='.$this->nasaApiClient->api_key, $this->nasaApiClient->getSounds());
     }
